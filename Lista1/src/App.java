bb0b93ef0779e3be9d01984f53412ebcbebff6ae
xPLACEBOx <joaodogma27@gmail.com>
2023-08-23 20:41:23 -0300

    Adicionar arquivos da lista01

--- Lista1/src/App.java
+++ Lista1/src/App.java
@@ -3,39 +3,19 @@ public class App {
     public static void main(String[] args) throws Exception {
         boolean operation=true;
         Scanner read=new Scanner(System.in);
-        cabesalho lista=new cabesalho();
-        
         while(operation){
-            lista.lista_exercicio();
-            int escolha=read.nextInt();
-
-            switch(escolha){
-                case 1:  
-                    atividade1 atv1=new atividade1();                  
-                    atv1.resolucao();    
-                    break;     
-                case 2:
-                    atividade1 atv2=new atividade1();
-                   atv2.resolucao2();  
-                   break;
-                case 3:
-                   atividade3 atv3=new atividade3();
-                   atv3.resolucao();
-                   break;  
-                case 4:
-                    atividade4 atv4=new atividade4();
-                    atv4.resolucao();
-                    break;
-                case 5:
-                   atividade5 atv5=new atividade5();
-                   atv5.resolucao();    
-                   break; 
-                case 6:
-                               
-                case 13:
-                    operation=false;
-                break;
-            }
+              lista(operation);
         }
+        
+        
+    }
+    public static void lista(boolean operation){
+     
+      if(operation==true){
+               cabesalho lista=new cabesalho();
+               lista.lista_exercicio();
+      }
+               
+        
     }
 }
--- Lista1/src/cabesalho.java
+++ Lista1/src/cabesalho.java
@@ -1,4 +1,7 @@
+import java.util.Scanner;
 public class cabesalho {
+    String escolha;
+    Scanner read=new Scanner(System.in);
     void lista_exercicio(){
         System.out.println("\nESCOLHA UMA LISTA DOS EXÉRCICIOS \n");
         System.out.println("1 - ATIVIDADE REAL PARA DOLAR \n");
@@ -9,10 +12,49 @@ public class cabesalho {
         System.out.println("6 - ATIVIDADE TROCA DE PÁPEIS\n");
         System.out.println(" 13- ENCERRA O PROGRAMA  ! ! !");
     }
-    public static String getVerficao(String validacao){
-        return validacao = 
+    void lista_escolha(String escolha ){
+
+        switch(escolha){
+            case "1":  
+                atividade1 atv1=new atividade1();                  
+                atv1.resolucao();    
+                lista.verficacao(escolha);
+            case "2":
+               atividade1 atv2=new atividade1();
+               atv2.resolucao2();  
+               lista.verficacao(escolha);
+               break;
+            case "3":
+               atividade3 atv3=new atividade3();
+               atv3.resolucao();
+               lista.verficacao(escolha);
+            case "4":
+                atividade4 atv4=new atividade4();
+                atv4.resolucao();
+                lista.verficacao(escolha);
+            case "5":
+               atividade5 atv5=new atividade5();
+               atv5.resolucao();    
+               lista.verficacao(escolha);
+            case "Y":
+               break;            
+            case "F":
+                System.out.println("Obrigado !");
+                operation=false;
+            break;
+        }
     }
-    void verficacao(){
-        System.out.println("Pressione Y - PARA CONTINUAR  ou R - RESETAR O EXERCÍCIO  ou F - PARA FINALIZAR !");
+    void verficacao(String escolha){
+        System.out.println("Pressione Y - VOLTAR PARA A LISTA  ou R - RESETAR O EXERCÍCIO  ou F - PARA FINALIZAR !");
+        escolha=read.next();
+        switch(escolha){  
+           case "Y":
+           case "R":
+             lista_escolha(escolha);
+           case "F":
+           boolean cancelado=false;
+           System.out.println("Obrigado por visitar meu programa :) ");
+            App.lista(cancelado);          
+        }
     }
 }
